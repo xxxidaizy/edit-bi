d040a6f4931895ef92addd5ed1c6cda03545d567
zhangqi <zhangqi@neolix.cn>
2021-11-25 17:42:52 +0800

    init success

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -3,10 +3,16 @@ import '../styles/homepage.less'
 
 const HomePage = () => {
   return (
-    <div className="home-page">
-      <div>Dashboard</div>
-      <div>View</div>
-      <div>Panel</div>
+    <div className="home-page flex">
+      <div className="home-page__dashboard">
+        Dashboard
+      </div>
+      <div className="home-page__view">
+        View
+      </div>
+      <div className="home-page__panel">
+        Panel
+      </div>
     </div>
   )
 }
